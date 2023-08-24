6ef6eb0288eddda4d750f61f3d9429154eb55119
morganfeeney <mfeeney@macrium.com>
2023-08-24 16:27:52 +0100

    Add test page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,6 +40,7 @@ export default function Home() {
             </a>
           </div>
         </div>
+            <Link className={styles.bug} href="/page2">CLICK ME</Link>
 
         <div className={styles.center}>
           <Image
